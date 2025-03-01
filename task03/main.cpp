b45d42c39c74336a6196c0f0421a29d7abadc7e2
Global Arts <chernov.drew@gmail.com>
2025-03-01 14:46:43 +0300

    changes

--- task03/main.cpp
+++ task03/main.cpp
@@ -4,22 +4,32 @@
 */
 
 #include <iostream>
-int main()
-{
-    setlocale (LC_ALL, "RUS");
-    int x;
-    std::cout << "Введите двухзначное натуральное число меньше 99: " << "\n";
-    std::cin >> x; // ввод двухзначного натурального числа, меньше 99
-    int xf = x / 10;
-    int xs = x % 10;
-    if (xf > xs) {
-        std::cout << xf << " - большая цифра" << std::endl;
-        std::cout << xs << " - меньшая цифра" << std::endl;
-    } else if (xs > xf) {
-        std::cout << xs << " - большая цифра" << std::endl;
-        std::cout << xf << " - меньшая цифра" << std::endl;
-    } else {
-        std::cout << "цифры равны" << std::endl;
+#include <algorithm> // Для функций min и max
+
+int main() {
+    int number;
+
+    // Ввод числа
+    std::cout << "Введите число не большее 99: ";
+    std::cin >> number;
+
+    // Проверка на диапазон
+    if (number < 10 || number > 99) {
+        std::cout << "Ошибка: введите двухзначное натуральное число." << std::endl;
+        return 1; // Завершение программы с ошибкой
     }
+
+    // Извлечение цифр
+    int tens = number / 10; // Десятки
+    int units = number % 10; // Единицы
+
+    // Определение наибольшей и наименьшей цифры с использованием min и max
+    int maxDigit = max(tens, units);
+    int minDigit = min(tens, units);
+
+    // Вывод результата
+    std::cout << maxDigit << " - большая цифра" << std::endl;
+    std::cout << minDigit << " - меньшая цифра" << std::endl;
+
     return 0;
 }
